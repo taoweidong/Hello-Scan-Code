148d347d710bc887fb6d85566a2767b4b76b3f6b
User <user@example.com>
2025-10-07 15:34:12 +0800

    删除冗余入口文件，优化主程序入口以支持click命令行界面

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 """
 Hello-Scan-Code 顶层入口
 """
@@ -10,4 +11,5 @@ sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'src'))
 if __name__ == "__main__":
     # 导入并运行主程序
     from src.main import main
-    sys.exit(main())
+    # 调用click命令
+    main()
